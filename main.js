0fdb6270b9b8b8a5849ea83893a59ff723a10510
UrielRossi7 <89169657+UrielRossi7@users.noreply.github.com>
2025-04-14 19:44:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+function Planes(){
+    const table = document.getElementById('container');
+    if (table.style.display == 'none'){
+        table.style.display = 'block'
+    }else{
+        table.style.display = 'none'
+    }
+}
+
+function MPagos(){
+    const pagos = document.getElementById('containerMP');
+    if (pagos.style.display == 'none'){
+        pagos.style.display = 'block'
+    }else{
+        pagos.style.display = 'none'
+    }
+}
+
+function Mayores() {
+    const mayores = document.getElementById('Tplan')
+    if (mayores.style.display === 'none'){
+        mayores.style.display = 'block'
+    }else{
+        mayores.style.display = 'none'
+    }
+}
+
+function Menores(){
+    const menores = document.getElementById('Tplan1');
+    if (menores.style.display==='none'){
+        menores.style.display = 'block'
+    }else{
+        menores.style.display = 'none'
+    }
+}
+
+function Filial(){
+    const filial = document.getElementById('Tplan2');
+    if (filial.style.display==='none'){
+        filial.style.display = 'block'
+    }else{
+        filial.style.display = 'none'
+    }
+}
+
+function Grupo(){
+    const grupo = document.getElementById('Tplan3');
+    if (grupo.style.display==='none'){
+        grupo.style.display = 'block'
+    }else{
+        grupo.style.display = 'none'
+    }
+}
+
+function Formulario(){
+    const form = document.getElementById('form-container')
+    if (form.style.display == 'none'){
+        form.style.display = 'block'
+    }else{
+        form.style.display = 'none'
+    }
+}
+
+function formc(){
+    const formuc = document.getElementById('form-container1')
+    if (formuc.style.display == 'none'){
+        formuc.style.display= 'block'
+    }else{
+        formuc.style.display = 'none'
+    }
+}
